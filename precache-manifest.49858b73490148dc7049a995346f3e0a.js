d9f99fef149247bb76335ee341d633be393ec478
kyerranorton <k.n.norton@wustl.edu>
2022-04-24 18:26:27 -0500

    Updates

---
+++ precache-manifest.49858b73490148dc7049a995346f3e0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17c3a8786c26c4b5dc00",
+    "url": "/09-todo-react-kyerranorton/static/css/main.d528faf6.chunk.css"
+  },
+  {
+    "revision": "17c3a8786c26c4b5dc00",
+    "url": "/09-todo-react-kyerranorton/static/js/main.17c3a878.chunk.js"
+  },
+  {
+    "revision": "939cbde5f9c76620fa49",
+    "url": "/09-todo-react-kyerranorton/static/js/1.939cbde5.chunk.js"
+  },
+  {
+    "revision": "7e1ddfb2ebbfc6854624",
+    "url": "/09-todo-react-kyerranorton/static/js/runtime~main.7e1ddfb2.js"
+  },
+  {
+    "revision": "fe5c2acfd39ad1f5ae1675a83a704d99",
+    "url": "/09-todo-react-kyerranorton/index.html"
+  }
+];
